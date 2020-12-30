4ee3d0672f058e795fc0ce8d0e0b88119dd963bd
Porksoduh <43007516+Porksoduh@users.noreply.github.com>
2020-12-29 21:11:22 -0500

    update config.h
    updated bltouch offset and margin

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -999,11 +999,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -6, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -6, -1.35 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
+#define PROBING_MARGIN 15
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED (133*60)
