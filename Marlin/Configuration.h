f27a30cc25b387f9d740468820a2bab9d56cea44
Porksoduh <43007516+Porksoduh@users.noreply.github.com>
2021-01-15 22:00:19 -0500

    update config.h
    ste steps/mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -744,7 +744,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 409 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80.27, 402.01, 409 }
 
 /**
  * Default Max Feed Rate (mm/s)
